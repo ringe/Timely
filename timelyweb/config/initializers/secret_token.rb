01b9d466cfac336f48e79ed95ce01bddf83ea67f
Runar Ingebrigtsen <runar@rin.no>
2011-11-09 11:29:10 +0100

    Rails with RSpec, Cucumber, Devise and CanCan

---
+++ timelyweb/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timelyweb::Application.config.secret_token = '98de8b03c16970cfb2b9334afba1d9940e5799fe408d7cc3ea915649b60aa3207071e25214dc84bc889654519d4566921acc117796f95acc03246b2604f394af'
