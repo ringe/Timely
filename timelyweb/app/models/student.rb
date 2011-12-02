47b354606b134dfcdabdeea041bca173f0e810a4
Kim-Eirik <kimma@KimmaLaptop.(none)>
2011-12-02 15:32:03 +0100

    Removed unneccesary code from student.rb

--- timelyweb/app/models/student.rb
+++ timelyweb/app/models/student.rb
@@ -1,5 +1,3 @@
 class Student < Person
     # Group has groups or is in group
-  has_many :sub_groups, class_name: "Group", foreign_key: "parent_id"
-  belongs_to :parent, class_name: "Group", foreign_key: "parent_id"
 end
